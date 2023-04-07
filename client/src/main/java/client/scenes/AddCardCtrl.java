0e8af7e726b7820da030fb5729308437742ddbf9
Dani de Dios Allegue <d.dediosallegue-1@student.tudelft.nl>
2023-04-07 17:47:11 +0200

    Introduced scroll pane

--- client/src/main/java/client/scenes/AddCardCtrl.java
+++ client/src/main/java/client/scenes/AddCardCtrl.java
@@ -15,6 +15,7 @@ import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.input.*;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import org.springframework.messaging.simp.stomp.*;
 
 import javax.inject.*;
@@ -44,7 +45,7 @@ public class AddCardCtrl implements InstanceableComponent {
     public ListView<Parent> taskBox;
 
     @FXML
-    public HBox tagBox;
+    public VBox tagBox;
 
     @FXML
     public Button tagButton;
